2e5d81d3ce75ba03f5611b0ee451066069917259
Marko Jekrić <markojerkic266@gmail.com>
2024-01-21 22:09:23 +0100

    git telescope

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,5 +1,82 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 
+local actions = require('telescope.actions')
+local action_state = require('telescope.actions.state')
+local finders = require('telescope.finders')
+local pickers = require('telescope.pickers')
+local sorters = require('telescope.sorters')
+local utils = require('telescope.utils')
+
+local git_switch = function(opts)
+	opts = opts or {}
+
+	local results = utils.get_os_command_output({ 'git', 'branch' })
+	local branches = results
+
+	pickers.new(opts, {
+		prompt_title = 'Git Switch',
+		finder = finders.new_table {
+			results = branches,
+			entry_maker = function(line)
+				return {
+					value = line,
+					ordinal = line,
+					display = line,
+				}
+			end,
+		},
+		sorter = sorters.get_generic_fuzzy_sorter(),
+		attach_mappings = function(prompt_bufnr, map)
+			local switch_selection = function()
+				local selection = action_state.get_selected_entry()
+				actions.close(prompt_bufnr)
+				local selected_branch = selection.value:gsub('%s+', ''):gsub('%*', '')
+				vim.cmd('Git switch ' .. selected_branch)
+			end
+
+			-- Use telescope actions to handle key mappings
+			actions.select_default:replace(switch_selection)
+
+			return true
+		end,
+	}):find()
+end
+
+local git_switch_remote = function(opts)
+	opts = opts or {}
+
+	local results = utils.get_os_command_output({ 'git', 'branch', '-r' })
+	local branches = results
+
+	pickers.new(opts, {
+		prompt_title = 'Git Switch Remote',
+		finder = finders.new_table {
+			results = branches,
+			entry_maker = function(line)
+				return {
+					value = line,
+					ordinal = line,
+					display = line,
+				}
+			end,
+		},
+		sorter = sorters.get_generic_fuzzy_sorter(),
+		attach_mappings = function(prompt_bufnr, map)
+			local switch_selection = function()
+				local selection = action_state.get_selected_entry()
+				actions.close(prompt_bufnr)
+				local selected_branch = selection.value:gsub('%s+', ''):gsub('%*', '')
+				vim.cmd('Git switch ' .. selected_branch)
+			end
+
+			-- Use telescope actions to handle key mappings
+			actions.select_default:replace(switch_selection)
+
+			return true
+		end,
+	}):find()
+end
+
 local Marko_Fugitive = vim.api.nvim_create_augroup("Marko_Fugitive", {})
 
 local autocmd = vim.api.nvim_create_autocmd
@@ -22,7 +99,9 @@ autocmd("BufWinEnter", {
             vim.cmd.Git({ 'pull --rebase' })
         end, opts)
 
-        vim.keymap.set("n", "<leader>fg", ":Telescope git_branches<CR>", opts)
+        vim.keymap.set("n", "<leader>fg", function ()
+           git_switch()
+        end, opts)
 
         -- NOTE: It allows me to easily set the branch i am pushing and any tracking
         -- needed if i did not set the branch up correctly
@@ -31,3 +110,4 @@ autocmd("BufWinEnter", {
         vim.keymap.set("n", "<leader>gs", ":Git switch -c ", opts);
     end,
 })
+
--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -16,6 +16,11 @@ require 'nvim-treesitter.configs'.setup {
     enable = true,
     -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
     disable = function(lang, buf)
+      -- true if location of file contains 'UagDai' in path
+      if vim.api.nvim_buf_get_name(buf):find("/tmp") then
+        return true
+      end
+
       local max_filesize = 100 * 1024 -- 100 KB
       local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
       if ok and stats and stats.size > max_filesize then
