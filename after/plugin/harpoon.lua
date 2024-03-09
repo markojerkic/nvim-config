13b9e3c1b235ffd04ebdbd89b640aa7d46a7c6d4
Marko Jekrić <markojerkic266@gmail.com>
2024-03-09 21:07:35 +0100

    format

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -2,44 +2,44 @@ local harpoon = require("harpoon")
 local keymap = vim.keymap.set
 
 local settings = {
-	-- saves the harpoon file upon every change. disabling is unrecommended.
-	save_on_change = true,
+    -- saves the harpoon file upon every change. disabling is unrecommended.
+    save_on_change = true,
 
-	-- sets harpoon to run the command immediately as it's passed to the terminal when calling `sendCommand`.
-	enter_on_sendcmd = false,
+    -- sets harpoon to run the command immediately as it's passed to the terminal when calling `sendCommand`.
+    enter_on_sendcmd = false,
 
-	-- closes any tmux windows harpoon that harpoon creates when you close Neovim.
-	tmux_autoclose_windows = false,
+    -- closes any tmux windows harpoon that harpoon creates when you close Neovim.
+    tmux_autoclose_windows = false,
 
-	-- filetypes that you want to prevent from adding to the harpoon list menu.
-	excluded_filetypes = { "harpoon" },
+    -- filetypes that you want to prevent from adding to the harpoon list menu.
+    excluded_filetypes = { "harpoon" },
 
-	-- set marks specific to each git branch inside git repository
-	mark_branch = false,
+    -- set marks specific to each git branch inside git repository
+    mark_branch = false,
 };
 
 harpoon:extend({
-  UI_CREATE = function(cx)
-    vim.keymap.set("n", "<C-v>", function()
-      harpoon.ui:select_menu_item({ vsplit = true })
-    end, { buffer = cx.bufnr })
+    UI_CREATE = function(cx)
+        vim.keymap.set("n", "<C-v>", function()
+            harpoon.ui:select_menu_item({ vsplit = true })
+        end, { buffer = cx.bufnr })
 
-    vim.keymap.set("n", "<C-x>", function()
-      harpoon.ui:select_menu_item({ split = true })
-    end, { buffer = cx.bufnr })
+        vim.keymap.set("n", "<C-x>", function()
+            harpoon.ui:select_menu_item({ split = true })
+        end, { buffer = cx.bufnr })
 
-    vim.keymap.set("n", "<C-t>", function()
-      harpoon.ui:select_menu_item({ tabedit = true })
-    end, { buffer = cx.bufnr })
-  end,
+        vim.keymap.set("n", "<C-t>", function()
+            harpoon.ui:select_menu_item({ tabedit = true })
+        end, { buffer = cx.bufnr })
+    end,
 })
 
 -- REQUIRED
 harpoon:setup({
-	settings = {
-		save_on_toggle = true,
-	},
-	global_settings = settings,
+    settings = {
+        save_on_toggle = true,
+    },
+    global_settings = settings,
 })
 -- REQUIRED
 
