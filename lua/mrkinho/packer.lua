8efa698aa942bd36e34d4b278e2beef63dae9e7d
Marko Jekrić <markojerkic266@gmail.com>
2024-03-24 21:21:04 +0100

    dap-ui dependency

--- lua/mrkinho/packer.lua
+++ lua/mrkinho/packer.lua
@@ -52,7 +52,7 @@ return require('packer').startup(function(use)
 
     -- DAP
     use 'mfussenegger/nvim-dap'
-    use { "rcarriga/nvim-dap-ui", requires = { "mfussenegger/nvim-dap" } }
+    use { "rcarriga/nvim-dap-ui", requires = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" } }
 
     -- Comments
     use {
