830b654cf11f294e0665e600eb2f3f9fc59c1123
Marko Jerkić <markojerkic266@gmail.com>
2024-05-15 11:43:18 +0200

    fix: treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,16 +1,82 @@
 return {
     {
         "nvim-treesitter/nvim-treesitter",
+        version = false,
+        cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
         dependencies = {
-            "nvim-treesitter/nvim-treesitter-context"
+            "nvim-treesitter/nvim-treesitter-context",
+            "nvim-treesitter/nvim-treesitter-textobjects"
         },
         build = ":TSUpdate",
-        branch = "main",
         lazy = false,
-        config = function()
-            require 'nvim-treesitter'.setup({
-                ensure_installed = { "javascript", "typescript", "tsx", "java", "c", "lua", "vim" },
-            })
+
+        ---@type TSConfig
+        ---@diagnostic disable-next-line: missing-fields
+        opts = {
+            highlight = { enable = true },
+            indent = { enable = true },
+            ensure_installed = {
+                "bash",
+                "c",
+                "diff",
+                "html",
+                "javascript",
+                "jsdoc",
+                "json",
+                "jsonc",
+                "lua",
+                "luadoc",
+                "luap",
+                "markdown",
+                "markdown_inline",
+                "python",
+                "query",
+                "regex",
+                "toml",
+                "tsx",
+                "typescript",
+                "vim",
+                "vimdoc",
+                "xml",
+                "yaml",
+            },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<C-space>",
+                    node_incremental = "<C-space>",
+                    scope_incremental = false,
+                    node_decremental = "<bs>",
+                },
+            },
+            textobjects = {
+                move = {
+                    enable = true,
+                    goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer" },
+                    goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer" },
+                    goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer" },
+                    goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer" },
+                },
+            },
+        },
+
+        config = function(i, opts)
+            if type(opts.ensure_installed) == "table" then
+                ---@type table<string, boolean>
+                local added = {}
+                opts.ensure_installed = vim.tbl_filter(function(lang)
+                    if added[lang] then
+                        return false
+                    end
+                    added[lang] = true
+                    return true
+                end, opts.ensure_installed)
+            end
+            local configs = require("nvim-treesitter.configs")
+            configs.setup(opts)
+            vim.schedule(function()
+                require("lazy").load({ plugins = { "nvim-treesitter-textobjects" } })
+            end)
         end,
     }
 }
