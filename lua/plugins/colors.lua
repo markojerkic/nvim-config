226675750c5746c87b7e5a8b7bf00ef69c9eacc6
Marko Jerkić <markojerkic266@gmail.com>
2024-07-14 12:35:24 +0200

    add new colorscheme

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -2,15 +2,25 @@ return {
     {
         "folke/tokyonight.nvim",
     },
-    { "catppuccin/nvim", name = "catppuccin" },
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        -- optionally set the colorscheme within lazy config
+        init = function()
+            vim.cmd([[colorscheme catppuccin-mocha]])
+            local util = require("marko.util.colors")
+            util.colourMyPencils()
+        end
+    },
     {
         "rose-pine/neovim",
         name = "rose-pine",
         lazy = false,
-        config = function()
-            vim.cmd([[colorscheme rose-pine]])
-            local util = require("marko.util.colors")
-            util.colourMyPencils()
-        end
+    },
+    {
+        "olivercederborg/poimandres.nvim",
+    },
+    {
+        "rebelot/kanagawa.nvim",
     }
 }
