1d528a8eaf4aec6dcf77abbd701ba31c0662a944
Marko Jerkić <markojerkic266@gmail.com>
2024-10-09 23:02:17 +0200

    c-a for completion accept

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -4,7 +4,7 @@ return {
         vim.g.copilot_no_tab_map = true
         vim.g.copilot_assume_mapped = true
 
-        vim.api.nvim_set_keymap("i", "<C-y>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+        vim.api.nvim_set_keymap("i", "<C-a>", 'copilot#Accept("<Tab>")', { silent = true, expr = true })
 
         vim.api.nvim_set_keymap("i", "<C-p>", 'copilot#Previous()', { silent = true, expr = true })
         vim.api.nvim_set_keymap("i", "<C-n>", 'copilot#Next()', { silent = true, expr = true })
