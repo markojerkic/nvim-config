33743ec7d2815b59d59d8dce2756c3c7ee55ea8e
Marko Jerkić <markojerkic266@gmail.com>
2024-12-06 23:46:17 +0100

    git status in dashboard

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,16 +1,3 @@
----@class snacks.Config
----@field bigfile? snacks.bigfile.Config | { enabled: boolean }
----@field gitbrowse? snacks.gitbrowse.Config
----@field lazygit? snacks.lazygit.Config
----@field notifier? snacks.notifier.Config | { enabled: boolean }
----@field quickfile? { enabled: boolean }
----@field statuscolumn? snacks.statuscolumn.Config  | { enabled: boolean }
----@field styles? table<string, snacks.win.Config>
----@field dashboard? snacks.dashboard.Config  | { enabled: boolean }
----@field terminal? snacks.terminal.Config
----@field toggle? snacks.toggle.Config
----@field win? snacks.win.Config
----@field words? snacks.words.Config
 local config = {
     dashboard = {
         enabled = true,
@@ -18,12 +5,25 @@ local config = {
         sections = {
             { section = "header" },
             { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
-            { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
-            { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+            { icon = " ", title = "Recent Files", cwd = true, section = "recent_files", indent = 2, padding = 1 },
+            {
+                icon = " ",
+                title = "Git Status",
+                section = "terminal",
+                enabled = function()
+                    return Snacks.git.get_root() ~= nil
+                end,
+                cmd = "git status --short --branch --renames",
+                height = 5,
+                padding = 1,
+                ttl = 5 * 60,
+                indent = 3,
+            },
+
             { section = "startup" },
         },
     },
-    notifier = { enabled = false },
+    notifier = { enabled = true },
     quickfile = { enabled = false },
     statuscolumn = { enabled = false },
     words = { enabled = false },
