a1858271b8245ae2608602939c4a038069d1a3f5
Marko Jerkić <markojerkic266@gmail.com>
2025-01-15 17:44:27 +0100

    start config refactor

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -1,11 +1,5 @@
 return {
     {
-        'vrischmann/tree-sitter-templ',
-        config = function()
-            vim.filetype.add({ extension = { templ = "templ" } })
-        end
-    },
-    {
         'leoluz/nvim-dap-go'
     }
 }
